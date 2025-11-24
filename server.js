286415e3f77d5c7f61bd89055a9ec670cb8bed1b
rupanmentor <rupanguvimentor@gmail.com>
2025-11-24 20:12:50 +0530

    express completed

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 import http from "http";
 import dotenv from "dotenv";
+import express from "express";
 
 //configure dotenv package
 
 dotenv.config();
 
+//initialization of express
+
+const app = express();
+
+//express default middleware
+
+app.use(express.json()); // used to access the body content/payload from the request or client
+
+
 // port to run the server
 
 const port = process.env.PORT || 5000;
 
-//create a server using http & nodejs
 
+//Default route to avoid cannot get
+
+app.get("/", (req,res)=>{
+    res.status(200).send(`<h1 style="text-align:center;">Welcome to Backend </h1>`)
+})
+
+
+
+//create a server using http & nodejs
+/*
 http
   .createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/plain" });
@@ -18,5 +37,10 @@ http
     res.end();
   })
   .listen(port, () => {
-    console.log(`server is started and running on the port ${port}`);
+    console.log(`server is started and running on the port `);
   });
+*/
+
+app.listen(port,()=>{
+    console.log("server started");   
+})
