08f170529a1c21065d85255f6dde6df4f44bbb02
rupanmentor <rupanguvimentor@gmail.com>
2025-11-24 19:37:48 +0530

    nodemon & dotenv completed

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import http from "http";
+import dotenv from "dotenv";
+
+//configure dotenv package
+
+dotenv.config();
 
 // port to run the server
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //create a server using http & nodejs
 
 http
   .createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/plain" });
-    res.write("Hello Welcome to backend");
+    res.write("Hey all Welcome to the backend");
     res.end();
   })
   .listen(port, () => {
